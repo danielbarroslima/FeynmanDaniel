48f36fb90d2ebe779af6f51ee4f7dac71d2a71ca
Daniel Barros Lima <danbarros@uninove.edu.br>
2017-05-25 00:48:01 -0300

    Telas iniciais do video 1, COMO SE TORNAR UM MESTRE NOS ESTUDOS | A TÉCNICA FEYNMAN  inserido duas telas para os questionamentos

--- Av2-Trabalho/Form1.Designer.cs
+++ Av2-Trabalho/Form1.Designer.cs
@@ -28,57 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnGerar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.txtArtigo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGerar
             // 
-            this.button1.Location = new System.Drawing.Point(78, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGerar.Location = new System.Drawing.Point(41, 274);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar artigo";
+            this.btnGerar.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(262, 131);
+            this.button2.Location = new System.Drawing.Point(518, 274);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "A diante";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // txtArtigo
             // 
-            this.button3.Location = new System.Drawing.Point(482, 171);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.txtArtigo.Location = new System.Drawing.Point(73, 30);
+            this.txtArtigo.Multiline = true;
+            this.txtArtigo.Name = "txtArtigo";
+            this.txtArtigo.Size = new System.Drawing.Size(508, 220);
+            this.txtArtigo.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(657, 450);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtArtigo);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGerar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGerar;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtArtigo;
     }
 }
 
